de2eadd0c576719754bf6dea9468d78d659334aa
Divine <divinetaku@yahoo.com>
2024-09-28 09:48:16 -0400

    complete editing of test.sh file at line 22

---
+++ hit.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl  -X PUT -u "mpongdivine@gmail.com:XkO2YJUvaof4fsvLAqtM63EA"--data
---
+++ test.sh
@@ -0,0 +1,13 @@
+Hi
+Hello how are you
+This is devops training
+From Divine
+
+very pssionate in all he does
+
+ you can trust him 
+
+
+version=1.2.3.4
+
+tenth session will be handson
